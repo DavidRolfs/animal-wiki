c61dcbb5c89cfa4a65262956ee9c56ecf976bcde
david rolfs and guy anderson <david@>
2017-04-27 10:20:00 -0700

    link if else statmet to html images

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,23 @@
+
+
+
+
 $(document).ready(function(){
 
+  var clicked;
 
+  $("img").click(function() {
+    clicked = $(this)[0].id;
+    show(clicked)
+  });
 
-  
+  var show = function(anything){
+    if(anything === "tigerImg"){
+      $("#tiger").toggle();
+    } else if(anything === "snakeImg"){
+      $("#snake").toggle();
+    } else if(anything === "turtleImg"){
+      $("#turtle").toggle();
+    }
+  };
 })
